a1f4225e09fa764b9630e5f29440e21aa0abbadd
DinaDwiLestari729 <dinadwilestari.9020@gmail.com>
2026-06-09 03:27:22 +0700

    Menambahkan profile_scree

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profil"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              child: Icon(Icons.person, size: 50),
+            ),
+            SizedBox(height: 16),
+            Text(
+              "Mahasiswa",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text("mahasiswa@email.com")
+          ],
+        ),
+      ),
+    );
+  }
+}
